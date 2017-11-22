07814f9ee9ff92735f569075306a2dc7edb30159
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-22 23:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var beatles = [];
   for(var i = 0; i < musicians.length; i++){
   
-    beatles.push(`${musicians[i]} plays the ${instruments[i]}`)
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return beatles;
 }
